e70915a0a83b2ee1b3ef135de64b05976c262038
Marc92020 <75702334+Marc92020@users.noreply.github.com>
2021-07-03 18:05:18 -0400

    Create jscode.js

---
+++ jscode.js
@@ -0,0 +1,7 @@
+var rate;
+
+
+rate = '0';
+
+rate = Number(window.prompt('From 1 to 10, how do you rate the Windows 11 preview?'));
+window.alert('Thank you for the feedback! Current rating: ' + String(rate));
